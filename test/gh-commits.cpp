b64079a019fd95568b55b818e7765b1c4d0d8090
Yannic Staudt <yannic@tipi.build>
2022-12-02 13:44:33 +0000

    :wrench: typo

--- test/gh-commits.cpp
+++ test/gh-commits.cpp
@@ -1,4 +1,4 @@
-#include <gh/refs.hxx>
+#include <gh/commits.hxx>
 
 #include <cstdlib>
 #include <pre/json/to_json.hpp>
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
 
   gh::get_commit("cpp-pre", "json", "29a0a7e9770aba4e04413c5245536bb0e2400285", [](gh::commits::commit_t&& commit) {
-    std::cout << pre::json::to_json(refs).dump(2) << std::endl;
+    std::cout << pre::json::to_json(commit).dump(2) << std::endl;
   });
 
   return 0;

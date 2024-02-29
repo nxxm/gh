5bfeffade3b95d7640ab57d1e251cbf24ff0bc2a
Yannic Staudt <yannic@tipi.build>
2024-02-29 16:24:19 +0100

    :wrench: fixing gh-branches test to use llvm-project to test for the is_protected property rewriting as non-premium github orgs (like tipibuild/) cannot have protected branches (anymore?)

--- test/gh-branches.cpp
+++ test/gh-branches.cpp
@@ -18,13 +18,14 @@ int main(int argc, char** argv) {
 
   gh::list_branches("llvm", "llvm-project", [](gh::repos::branches&& branches) {
     assertm(branches.size() > 100, "We expect this to query multiple pages");
-  }, auth);
 
-  // a repo with branch protections
-  gh::list_branches("tipibuild", "test-gh-client-release", [&](gh::repos::branches&& branches) {
-    
-    auto findit = std::find_if(branches.begin(), branches.end(), [](gh::repos::branch_t& b) { return b.is_protected; });
-    
+    // note on this:
+    // the reason this aspect is tested is that the DTO sent by github does contain a "protected" property which gh:: remaps to "is_protected"
+    // to circumnvent the use of the reserved C++ keyword in a struct/class.
+    // 
+    // We are expecting the llvm-project to have a few protected branches (e.g. at least main seems to be at
+    // time of writing). 
+    auto findit = std::find_if(branches.begin(), branches.end(), [](gh::repos::branch_t& b) { return b.is_protected; });    
     assertm(findit != branches.end(), "No protected branch found in repo / was expected");
   }, auth);
 
